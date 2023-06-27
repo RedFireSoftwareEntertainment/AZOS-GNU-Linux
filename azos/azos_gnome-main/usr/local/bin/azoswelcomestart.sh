a28ab44473fbd44a3e34d753ffe1f8a79830ec9a
RedFireSoftwareEntertainment <98542488+RedFireSoftwareEntertainment@users.noreply.github.com>
2023-06-27 14:27:08 +0300

    Update azoswelcomestart.sh

--- azos/azos_gnome-main/usr/local/bin/azoswelcomestart.sh
+++ azos/azos_gnome-main/usr/local/bin/azoswelcomestart.sh
@@ -4,7 +4,8 @@ if [ -e /usr/bin/azoswelcome.py ]
 then
     pip install pywebview
     python /usr/bin/azoswelcome.py
-    rm /usr/bin/azoswelcome.py 
+    rm /usr/bin/azoswelcome.py
+    pip uninstall pywebview
 else
     echo "azoswelcome.py was already executed once in this system so we skip it."
 fi
