10ea45c63b2248ba2469a7f82bfeaa9e87762228
RedFireSoftwareEntertainment <98542488+RedFireSoftwareEntertainment@users.noreply.github.com>
2023-08-13 21:49:01 +0300

    Update azoswelcomestart.sh

--- azos/azos_kde-main/usr/local/bin/azoswelcomestart.sh
+++ azos/azos_kde-main/usr/local/bin/azoswelcomestart.sh
@@ -2,13 +2,21 @@
 
 if [ -e /usr/bin/azoswelcome.py ]
 then
-    pip install pywebview
-    python /usr/bin/azoswelcome.py
-    rm /usr/bin/azoswelcome.py
-    pip uninstall pywebview
-    exit
+    pip install pywebview 
+    python /usr/bin/azoswelcome.py 
+    rm /usr/bin/azoswelcome.py 
+    pip uninstall pywebview 
+    exit 
 else
-    echo "azoswelcome.py was already executed once in this system so we skip it."
-    exit
+    echo "azoswelcome.py was already executed once in this system so we skip it." 
+    exit  
 fi
+
+# Startup Commands
+
+systemctl start esystemctl start example.service 
+freshclam 
+clamscan --recursive --infected /home/$USER 
+
+
 
