c2827965185024f8f3cbbedf57ed0cb4c0e53755
RedFireSoftwareEntertainment <98542488+RedFireSoftwareEntertainment@users.noreply.github.com>
2023-10-24 20:03:30 +0300

    Add files via upload

---
+++ azos/azos_kde-main/etc/azrepo/x86_64/makerepo.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Licenced under the GNU GPL 3.0
+# Part of AZOS GNU/Linux and all legal information from this legal page apply here: https://sites.google.com/view/azosofficialsite/legal
+
+repo-add azrepo.db.tar.gz *.pkg.tar.zst
